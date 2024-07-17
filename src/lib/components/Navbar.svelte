79c4ce42e3456e54de7af69de9969fc45ca1ed76
LoneWolf <imalkesara2001@gmail.com>
2024-07-17 20:00:37 +0530

    Navbar component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header class=" col-span-3 bg-daisyBush text-center">
+	<div>
+		<a href="/invoices"
+			><img src="/images/logo.svg" alt="the dollar hollar logo" class=" mx-auto" /></a
+		>
+	</div>
+
+	<nav>
+		<ul class="list-none text-2xl font-bold">
+			<li><a href="/invoices" class:active={$page.url.pathname === '/invoices'}>Invoices</a></li>
+			<li><a href="/clients" class:active={$page.url.pathname === '/clients'}>Clients</a></li>
+			<li><a href="#" class:active={$page.url.pathname === '#'}>Settings</a></li>
+			<li><a href="#">Logout</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="postcss">
+	nav ul li {
+		@apply mb-6;
+	}
+
+	nav ul li a {
+		@apply font-bold text-white hover:text-goldenFizz;
+	}
+
+	nav ul li a.active {
+		@apply text-robinEggBlue px-8 transition-[padding] duration-300;
+		background:
+			url('/images/active-nav--left.svg') left no-repeat,
+			url('/images/active-nav--right.svg') right no-repeat;
+	}
+	nav ul li a.active:hover {
+		@apply px-9;
+	}
+</style>
