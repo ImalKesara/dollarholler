f9d8e8ea41d56f7cf9b5d90e38dec9ca4983e408
LoneWolf <imalkesara2001@gmail.com>
2024-07-24 16:01:53 +0530

    two decimal number and convert amount to thousands

--- src/lib/utils/moneyHelpers.ts
+++ src/lib/utils/moneyHelpers.ts
@@ -2,3 +2,16 @@ export const sumLineItems = (lineItems: LineItems[] | undefined): number => {
 	if (!lineItems) return 0;
 	return lineItems.reduce((accur, curr) => accur + curr.amount, 0);
 };
+export const centsToDollers = (cents: number): string => {
+	const dolloars = cents / 100;
+	const addDecimal = twoDecimal(dolloars);
+	return addThousandsSeparator(addDecimal);
+};
+
+export const twoDecimal = (myNum: number): string => {
+	return myNum.toFixed(2);
+};
+
+export const addThousandsSeparator = (myNumb: string): string => {
+	return myNumb.replace(/\B(?=(\d{3})+(?!\d))/g, ','); //15,000 thousands format
+};
