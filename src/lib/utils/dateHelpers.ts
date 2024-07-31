4fc26c72060e3de1fd8de673f3fe787f57178d8d
LoneWolf <imalkesara2001@gmail.com>
2024-07-31 22:08:27 +0530

    describe functions of dateHelpers

--- src/lib/utils/dateHelpers.ts
+++ src/lib/utils/dateHelpers.ts
@@ -1,10 +1,21 @@
 import { isBefore } from 'date-fns';
 
+/**
+ *
+ * @param {string} myDate
+ * @returns {string} m-d-yyyy
+ */
 // this function takes yyyy-mm-dd then convert into mm-dd-yyyy
 export const convertDate = (myDate: string): string => {
 	const [year, month, date] = splitDate(myDate);
 	return `${parseInt(date)} / ${parseInt(month)} / ${parseInt(year)}`;
 };
+
+/**
+ * Determine whether a  date is before today (meaning it's late)
+ * @param {string} myDate
+ * @returns {boolean}
+ */
 export const islate = (myDate: string): boolean => {
 	const [year, month, date] = splitDate(myDate);
 	const dueDate = new Date(parseInt(year), parseInt(month), parseInt(date));
@@ -12,6 +23,11 @@ export const islate = (myDate: string): boolean => {
 	return isBefore(dueDate, today);
 };
 
+/**
+ *
+ * @param {string}myDate
+ * @returns {Array} yyyy-m-d
+ */
 export const splitDate = (myDate: string): Array<string> => {
 	return myDate.split('-');
 };
