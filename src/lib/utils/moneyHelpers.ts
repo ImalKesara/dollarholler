df1d3cbaff8797de7a5bd79857298f805e0d7503
LoneWolf <imalkesara2001@gmail.com>
2024-07-31 22:15:07 +0530

    Describe  moneyHelpers fucntions

--- src/lib/utils/moneyHelpers.ts
+++ src/lib/utils/moneyHelpers.ts
@@ -1,20 +1,41 @@
 import { invoices } from '$lib/stores/invoiceStore';
 
+/**
+ * Takes all the line items and adds them up (seed.json)
+ * @param {Array|undefined} lineItems
+ * @returns {number}
+ */
 //individual sum
 export const sumLineItems = (lineItems: LineItems[] | undefined): number => {
 	if (!lineItems) return 0;
 	return lineItems.reduce((accur, curr) => accur + curr.amount, 0);
 };
+
+/**
+ * Takes and returns a dollar amount (USD) , formatted with commas and 2 decimal places
+ * @param {number} cents
+ * @returns {string}
+ */
 export const centsToDollers = (cents: number): string => {
 	const dolloars = cents / 100;
 	const addDecimal = twoDecimal(dolloars);
 	return addThousandsSeparator(addDecimal);
 };
 
+/**
+ * Takes a number and returns the number with 2 decimal places
+ * @param {number} myNum
+ * @returns {string}
+ */
 export const twoDecimal = (myNum: number): string => {
 	return myNum.toFixed(2);
 };
 
+/**
+ * adds a thounsands operator
+ * @param {string} myNumb
+ * @returns {string}
+ */
 export const addThousandsSeparator = (myNumb: string): string => {
 	return myNumb.replace(/\B(?=(\d{3})+(?!\d))/g, ','); //15,000 thousands format
 };
