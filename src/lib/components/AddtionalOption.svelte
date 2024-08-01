d6881e17ebd25c19923bd25b91c77484382ad6dc
LoneWolf <imalkesara2001@gmail.com>
2024-08-02 00:11:56 +0530

    Addtional options

---
+++ src/lib/components/AddtionalOption.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import View from '$lib/components/Icon/View.svelte';
+</script>
+
+<ul class=" absolute left-0 top-4 rounded-lg bg-white shadow-lg">
+	<li>
+		<button
+			class="flex w-full gap-x-2 p-4 font-sansSerif font-bold text-pastelPurple hover:text-daisyBush"
+		>
+			<View />
+			Send
+		</button>
+	</li>
+	<li>
+		<button
+			class="flex w-full gap-x-2 p-4 font-sansSerif font-bold text-pastelPurple hover:text-daisyBush"
+		>
+			<View />
+			Send
+		</button>
+	</li>
+</ul>
+
+<style lang="postcss">
+	li {
+		@apply border-b-2 border-whisper;
+	}
+
+	li:last-child {
+		@apply border-b-0;
+	}
+</style>
